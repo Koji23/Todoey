52e41f3dfed074e57dc875b83c26be03fd65052d
Jordan Chong <jordan@aerofs.com>
2018-12-14 13:41:31 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jordan Chong on 12/14/18.
+//  Copyright © 2018 Jordan Chong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
